a0d5c5a6f414d1524ad28d1bcc551d4eddbdea79
Roland Geider <roland@geider.net>
2013-06-26 18:13:02 +0200

    Fix APGL headers

--- submission/context_processor.py
+++ submission/context_processor.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 
-# This file is part of Kumasta.
+# This file is part of the DBFV site.
 #
-# Kumasta is free software: you can redistribute it and/or modify
+# the DBFV site is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Kumasta is distributed in the hope that it will be useful,
+# the DBFV site is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with Kumasta.  If not, see <http://www.gnu.org/licenses/>.
+# along with the DBFV site.  If not, see <http://www.gnu.org/licenses/>.
 
 
 from submission.models import user_type
