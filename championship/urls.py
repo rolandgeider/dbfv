b42f303bc57d3affdd37b6683c8f98d735833848
Roland Geider <roland@geider.net>
2016-07-27 22:58:40 +0200

    Don't use the deprecated patterns in urls.py

--- championship/urls.py
+++ championship/urls.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 
 from championship.views import championships
 from championship.views import categories
@@ -23,7 +23,7 @@ from championship.views import placements
 
 
 # sub patterns for championships
-patterns_championship = patterns('',
+patterns_championship = [
 
     url(r'^liste/alle$',
         championships.ChampionshipListView.as_view(),
@@ -43,10 +43,10 @@ patterns_championship = patterns('',
     url(r'^(?P<pk>\d+)/export-csv',
         championships.export_participants,
         name='export-csv'),
-)
+]
 
 # sub patterns for categories
-patterns_categories = patterns('',
+patterns_categories = [
     url(r'^liste$',
         categories.CategoriesListView.as_view(),
         name='list'),
@@ -59,10 +59,10 @@ patterns_categories = patterns('',
     url(r'^(?P<pk>\d+)/loeschen$',
         categories.CategoryDeleteView.as_view(),
         name='delete'),
-)
+]
 
 # sub patterns for participations
-patterns_participation = patterns('',
+patterns_participation = [
   url(r'^(?P<pk>\d+)/loeschen$',
       participations.ParticipationDeleteView.as_view(),
       name='delete'),
@@ -72,10 +72,10 @@ patterns_participation = patterns('',
   url(r'^antrag-(?P<submission_pk>\d+)/anmelden$',  #JS!
       participations.ParticipationCreateView.as_view(),
       name='register'),
-)
+]
 
 # sub patterns for championship placements
-patterns_placement = patterns('',
+patterns_placement = [
   url(r'^(?P<pk>\d+)/bearbeiten$',
       placements.PlacementUpdateView.as_view(),
       name='edit'),
@@ -85,11 +85,11 @@ patterns_placement = patterns('',
   url(r'^antrag-(?P<participation_pk>\d+)/anmelden$',
       placements.PlacementCreateView.as_view(),
       name='register'),
-)
+]
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^', include(patterns_championship, namespace="championship")),
     url(r'^teilnahme/', include(patterns_participation, namespace="participation")),
     url(r'^platzierung/', include(patterns_placement, namespace="placement")),
     url(r'^kategorie/', include(patterns_categories, namespace="category")),
-)
+]
