03c78e93200e12aa818e39ff70467cdfd7942370
Roland Geider <roland@geider.net>
2026-02-09 15:01:08 +0100

    Use consistent formatting in license headers

--- core/urls.py
+++ core/urls.py
@@ -1,21 +1,20 @@
-# -*- coding: utf-8 *-*
-
-# Django
-from django.conf.urls import include
-from django.contrib.auth.decorators import permission_required
-# This file is part of Kumasta.
+# This file is part of the DBFV submission site
 #
-# Kumasta is free software: you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Kumasta is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
+
+# Django
+from django.conf.urls import include
+from django.contrib.auth.decorators import permission_required
 from django.urls import path
 from django.views.generic import TemplateView
 
